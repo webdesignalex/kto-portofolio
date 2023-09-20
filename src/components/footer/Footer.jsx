9199396cd94e2735862648c8d50c999c493e845e
kaimtowny <niniee61@gmail.com>
2023-09-20 10:03:26 +0630

    Footer Folder updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,54 @@
-import React from 'react'
-import './footer.css';
-import {FaFacebook} from 'react-icons/fa';
-import {FiInstagram} from 'react-icons/fi';
-import {IoLogoTwitter} from 'react-icons/io';
-
+import React from "react";
+import "./footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Khaing</a>
-
+      <a href="#" className="footer__logo">
+        Khaing Thazin Oo
+      </a>
 
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#skill">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#certificates">Certificates</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebook /></a>
-        <a href="https://instagram.com"><FiInstagram /></a>
-        <a href="https://twitter.com"><IoLogoTwitter /></a>
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
       </div>
 
       <div className="footer__copyright">
-        <small>
-          &copy; Khaing Tutorials. All rights reserved. 
-        </small>
+        <small>&copy; Khaing's portfolio. All rights reserved.</small>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
