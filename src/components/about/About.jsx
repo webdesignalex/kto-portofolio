9b265730a8653ed44721f6f7457b7a5cfe63b25d
kaimtowny <niniee61@gmail.com>
2023-09-20 10:05:59 +0630

    About Folder Updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './about.css';
-import ME from '../../assets/KTO.JPG';
-import {FaAward} from 'react-icons/fa';
-import {VscFolderLibrary} from 'react-icons/vsc';
+import React from "react";
+import "./about.css";
+import ME from "../../assets/KTO.JPG";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
@@ -15,38 +15,42 @@ const About = () => {
           <div className="about__me__image">
             <img src={ME} alt="About Image" />
           </div>
-
         </div>
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon' />
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Communication Experience</h5>
               <small>2+ Years Volunteering</small>
             </article>
-            <article className='about__card'>
-              <FaAward className='about__icon' />
+            <article className="about__card">
+              <FaAward className="about__icon" />
               <h5>Leadership Experience</h5>
               <small>3+ Years Volunteering</small>
             </article>
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon' />
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>3+ Completed</small>
+              <small>7+ Completed</small>
             </article>
           </div>
 
           <p>
-            An active learner who wants to overcome with new challenges and who is seeking an opportunity to explore the world with Software Development.
-            An active member who is always ready for any tasks in the team.
+            I am a third year student of MIIT majoring Computer Science and
+            Engineering. An active learner who wants to overcome with new
+            challenges and who is seeking an opportunity to explore the world
+            with Software Development. An active member who is always ready for
+            any tasks in the team.
           </p>
 
-          <a href="#contact" className="btn btn-primary"> Let's Talk</a>
-
+          <a href="#contact" className="btn btn-primary">
+            {" "}
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
