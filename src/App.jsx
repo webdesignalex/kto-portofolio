c3ba065f9b8765184a1fdd57f76f6f4217e76586
kaimtowny <niniee61@gmail.com>
2023-09-20 11:03:09 +0630

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Services from './components/services/Services';
-import Testimonials from './components/testimonials/Testimonials';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
-
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Skill from "./components/skill/Skill";
+import Projects from "./components/projects/Projects";
+import Certificates from "./components/certificates/Certificates";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <>
-    <Header />
-    <Nav />
-    <About />
-    <Experience />
-    <Services />
-    <Testimonials />
-    <Contact/>
-    <Footer />
+      <Header />
+      <Nav />
+      <About />
+      <Skill />
+      <Projects />
+      <Certificates />
+      <Contact />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
